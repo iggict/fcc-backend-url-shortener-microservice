99850fde4b38176de5af862e0f8f7fe7430290f5
iggict <58374304+iggict@users.noreply.github.com>
2021-11-12 08:58:30 +0000

    GET and POST for api/shorturl

--- server.js
+++ server.js
@@ -1,14 +1,50 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const validUrl = require("valid-url");
+// const shortId = require('shortid');
+require("dotenv").config();
+
 const app = express();
 
-const cors = require("cors");
+/** Set Up Mongoose **/
 
-// Middlewares
+mongoose.connect(process.env.MONGO_URI.trim(), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+/** Create a Model **/
+
+const urlSchema = new mongoose.Schema({
+  original_url: { type: String, required: true },
+  short_url: { type: Number, required: true },
+});
+
+const UrlModel = mongoose.model("url", urlSchema);
+
+/** Helper functions **/
+
+const isValidUrl = (url) => {
+  let result = true;
+  if (validUrl.isWebUri(url) === undefined) result = false;
+  return result;
+};
+
+// /**
+//  * shortId.generate() generates an alphanumeric
+//  * Id that is not compatible with freecodecamp user stories. */
+// const newShortUrl = () => {
+//  return shortId.generate();
+// };
+
+/** Set Up Express Middlewares **/
+
+app.use(express.urlencoded({ extended: false })); // request parser mw
 app.use(cors({ optionsSuccessStatus: 200 })); // cors mw
 app.use(express.static("public")); // assets mw
 
-// Routes
+/** Routes **/
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -16,6 +52,73 @@ app.get("/", (req, res) => {
 
 app.get("/api/hello", (req, res) => {
   res.json({ greeting: "hello API" });
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ test: newShortUrl2() });
+});
+
+// POST (shorturl)
+
+app.post("/api/shorturl", async (req, res) => {
+  const url = req.body.url;
+
+  if (!isValidUrl(url)) {
+    return res.json({ error: "invalid url" });
+  }
+
+  try {
+    let foundUrl = await UrlModel.findOne({ original_url: url });
+
+    if (!foundUrl) {
+
+      let maxShortUrl = await UrlModel
+        .find()
+        .sort({short_url: "desc"})
+        .limit(1)
+        .then(docs => {
+          if (docs.length) {
+            return docs[0].short_url;
+          } else {
+            return 0;
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+      
+      foundUrl = new UrlModel({
+        original_url: url,
+        short_url: maxShortUrl + 1,
+      });
+      await foundUrl.save();
+    }
+
+    res.json({
+      original_url: foundUrl.original_url,
+      short_url: foundUrl.short_url,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server error");
+  }
+});
+
+// GET (shorturl)
+
+app.get("/api/shorturl/:shortUrl?", async (req, res) => {
+  try {
+    const foundUrl = await UrlModel.findOne({
+      short_url: req.params.shortUrl,
+    });
+    if (!foundUrl) {
+      return res.status(404).json({ error: "no url found" });
+    }
+
+    res.redirect(foundUrl.original_url);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server error");
+  }
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
